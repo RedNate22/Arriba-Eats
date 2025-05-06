e367bf144fc72e1659f96fa45cb83971d963aa8d
RedNate22 <nathan.pithie@gmail.com>
2025-05-06 13:35:38 +1000

    commit

--- Arriba Eats/Core/Entities/User.cs
+++ Arriba Eats/Core/Entities/User.cs
@@ -4,12 +4,12 @@ namespace Entities
 {
     public abstract class User
     {
-        public string? Name { get; private set; }
+        public string Name { get; private set; }
         public int Age { get; private set; }
-        public string? Email { get; private set; }
-        public string? Mobile { get; private set; }
-        public string? Password { get; private set; }
-        public string? Location { get; private set; }
+        public string Email { get; private set; }
+        public string Mobile { get; private set; }
+        public string Password { get; private set; }
+        public string Location { get; private set; }
         
         protected User(string name, int age, string email, string mobile, string password, string location)
         {
